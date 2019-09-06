60f89171081b10f3c810f84a3adbe52f6afc1f92
July-4n <september4n@gmail.com>
2019-09-06 23:42:00 +0500

    добавлена переменная htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+var htmlmin = require("gulp-htmlmin");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
